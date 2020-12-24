c494172d8c076554926f9c2892745fbe68aa7d2e
heitorlimamorei <61924603+heitorlimamorei@users.noreply.github.com>
2020-12-24 11:13:43 -0300

    Melhoria do video.js
    Os arquivos do JavaScript do Videos.html foram separados.

--- video.js
+++ video.js
@@ -1,20 +1,31 @@
-let permissao;
- let permissao = window.prompt('sua senha ') 
+let permissao = window.prompt('Sua senha ')
 
-switch (permissao) {
-    case 'usuarioap':
-        console.log("Usuario aprovado")
-        alert('Bem Vindo UserAp!');
-        break;
-        
-    default:
-        console.log('Usuario negado')
-        alert('Ascesso Negado')
-        open(URL='https://www.youtube.com/watch?v=ZZXEMrzzsTU')
-        break;
-}
-function afd() {
+    switch (permissao) {
+        case 'usuarioap':
+            console.log("Usuario aprovado")
+            alert('Bem Vindo UserAp!');
+            document.write(`Logado com : ${permissao}`)
+            break;
+
+            case 'usuarioap1':
+                console.log("Usuario aprovado")
+                alert('Bem Vindo UserAp1')
+                document.write(`Logado com : ${permissao}`)
+                break;
+             case 'heitor':
+                console.log("Usuario aprovado")
+                alert('Bem Vindo UserAp2')
+                document.write(`Logado com : ${permissao}`)
+                break;    
+
+        default:
+            console.log('Usuario negado')
+            alert('Ascesso Negado')
+            document.writeln("USUARIO ILEGAL")
+            break;
+    }
+    function asd (){
     console.log("Pedido de Usuario")
     console.log(`Nome de usuario${permissao}`)
     alert(`Nome de usuario:${permissao} Status: Logado`)
-}
+    }
