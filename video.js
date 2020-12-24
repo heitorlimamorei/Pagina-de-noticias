8ba2e17bad23a88b0bb9e3f38cbf03d04d2d8caa
heitorlimamorei <61924603+heitorlimamorei@users.noreply.github.com>
2020-12-24 11:57:36 -0300

    Update video.js

--- video.js
+++ video.js
@@ -1,31 +1,37 @@
 let permissao = window.prompt('Sua senha ')
+let status
 
     switch (permissao) {
         case 'usuarioap':
             console.log("Usuario aprovado")
             alert('Bem Vindo UserAp!');
             document.write(`Logado com : ${permissao}`)
+            status = "logado";
             break;
 
             case 'usuarioap1':
                 console.log("Usuario aprovado")
                 alert('Bem Vindo UserAp1')
                 document.write(`Logado com : ${permissao}`)
+                status = "logado";
                 break;
              case 'heitor':
                 console.log("Usuario aprovado")
                 alert('Bem Vindo UserAp2')
                 document.write(`Logado com : ${permissao}`)
+                status = "logado";
                 break;    
 
         default:
             console.log('Usuario negado')
             alert('Ascesso Negado')
             document.writeln("USUARIO ILEGAL")
+            status = "não aprovado";
             break;
     }
     function ghu (){
     console.log("Pedido de Usuario")
     console.log(`Nome de usuario${permissao}`)
-    alert(`Nome de usuario:${permissao} Status: Logado`)
+    alert(`Nome de usuario:${permissao} Status: ${status}`)
+    
     }
