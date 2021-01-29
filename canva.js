ab2f0f2ebfc3f5c9af10182678b31e65ec0515ac
heitorlimamorei <61924603+heitorlimamorei@users.noreply.github.com>
2021-01-29 13:52:43 -0300

    Path de Melhoria
    Varias melhorias, add da nova home, desativação de icones ( devem ser instalados de forma externa),
    CSS, HTML, JS

--- canva.js
+++ canva.js
@@ -1,13 +1,32 @@
-
-function draw(){
-    var canvas = document.getElementById("canvas")
-    if(canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "rgb(200, 0, 0)";
-        ctx.fillRect (10, 10, 55, 50);
-
-        
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
-    }
+function touchdate() {
+    document.open(URL = "https://www.zwicon.com/how-to-use.html")
 }
+
+let permissao = window.prompt('Sua senha ')
+let status
+
+    switch (permissao) {
+        case 'usuarioap':
+            console.log("Usuario aprovado")
+            alert('Bem Vindo UserAp!');
+            status = "logado";
+            break;
+
+            case 'usuarioap1':
+                console.log("Usuario aprovado")
+                alert('Bem Vindo UserAp1')
+                status = "logado";
+                break;
+             case 'heitor':
+                console.log("Usuario aprovado")
+                alert('Bem Vindo UserAp2')
+                status = "logado";
+                break;    
+
+        default:
+            console.log('Usuario negado')
+            alert('Ascesso Negado')
+            document.writeln("USUARIO ILEGAL")
+            status = "não aprovado";
+            break;
+    }
